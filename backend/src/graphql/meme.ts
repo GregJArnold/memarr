02fc3aff45a0e949868207533110caef8684d499
Greg Arnold <greg@arnoldassociates.com>
2025-04-30 18:30:50 -0400

    Add in some meme queries and mutations.

--- backend/src/graphql/meme.ts
+++ backend/src/graphql/meme.ts
@@ -14,16 +14,67 @@ export const typeDefs = gql`
 		templateId: ID
 		userId: ID!
 		filePath: String!
+		pendingProcessing: Boolean!
+		tags: [Tag!]!
+		template: Template
+		texts: [MemeText!]!
+		url: String!
 		createdAt: DateTime!
 		updatedAt: DateTime!
 	}
 
+	type Query {
+		memes: [Meme!]!
+		meme(id: ID!): Meme
+	}
+
 	type Mutation {
 		uploadMeme(file: Upload!): Meme!
+		addTag(memeId: ID!, tagName: String!): Meme!
+		removeTag(memeId: ID!, tagId: ID!): Meme!
+		addTextBlock(memeId: ID!, text: String!, textBlockId: ID!): Meme!
+		updateTextBlock(memeId: ID!, textBlockId: ID!, text: String!): Meme!
+		deleteTextBlock(memeId: ID!, textBlockId: ID!): Meme!
 	}
 `;
 
+const withMeme = <TRoot, TArgs extends Record<string, unknown>>(
+	resolver: (root: TRoot, args: TArgs, ctx: AuthTransactionContext, meme: Meme) => Promise<unknown>
+) =>
+	withUser(
+		withTransaction(async (root: TRoot, args: {memeId: string} & TArgs, ctx: AuthTransactionContext) => {
+			const meme = await Meme.query(ctx.trx)
+				.findById(args.memeId)
+				.where("user_id", ctx.user.id)
+				.withGraphJoined("[tags, template, texts.[textBlock]]");
+
+			if (!meme) {
+				throw new Error("Meme not found");
+			}
+
+			return resolver(root, args, ctx, meme);
+		})
+	);
+
 export const resolvers = {
+	Meme: {
+		pendingProcessing: async (meme: Meme) => {
+			if (!meme.tasks) meme.tasks = await meme.$relatedQuery("tasks");
+
+			return meme.tasks.some(task => task.action === "process" && task.status === "pending");
+		},
+		url: (meme: Meme) => `http://localhost:4000/image/${meme.id}`,
+	},
+	Query: {
+		memes: withUser(
+			withTransaction(async (_: unknown, __: unknown, {user, trx}: AuthTransactionContext) => {
+				return Meme.query(trx)
+					.where("user_id", user.id)
+					.withGraphFetched("[tags, template, texts.[textBlock]]");
+			})
+		),
+		meme: withMeme(async (_, __, ___, meme) => meme),
+	},
 	Mutation: {
 		uploadMeme: withUser(
 			withTransaction(
@@ -54,5 +105,41 @@ export const resolvers = {
 				}
 			)
 		),
+		addTag: withMeme(async (_, {tagName}: {tagName: string}, {trx}, meme) => {
+			await meme.$relatedQuery("tags", trx).insert({name: tagName});
+			return meme.$query(trx).withGraphFetched("[tags, template, texts.[textBlock]]");
+		}),
+		removeTag: withMeme(async (_, {tagId}: {tagId: string}, {trx}, meme) => {
+			await meme.$relatedQuery("tags", trx).deleteById(tagId);
+			return meme.$query(trx).withGraphFetched("[tags, template, texts.[textBlock]]");
+		}),
+		addTextBlock: withMeme(
+			async (_, {text, textBlockId}: {text: string; textBlockId: string}, {trx}, meme) => {
+				await meme.$relatedQuery("texts", trx).insert({content: text, textBlockId});
+				return meme.$query(trx).withGraphFetched("[tags, template, texts.[textBlock]]");
+			}
+		),
+		updateTextBlock: withMeme(
+			async (_, {textBlockId, text}: {textBlockId: string; text: string}, {trx}, meme) => {
+				const textBlock = await meme.$relatedQuery("texts", trx).findById(textBlockId);
+
+				if (!textBlock) {
+					throw new Error("Text block not found");
+				}
+
+				await textBlock.$query(trx).patchAndFetch({content: text});
+				return meme.$query(trx).withGraphFetched("[tags, template, texts.[textBlock]]");
+			}
+		),
+		deleteTextBlock: withMeme(async (_, {textBlockId}: {textBlockId: string}, {trx}, meme) => {
+			const textBlock = await meme.$relatedQuery("texts", trx).findById(textBlockId);
+
+			if (!textBlock) {
+				throw new Error("Text block not found");
+			}
+
+			await textBlock.$query(trx).delete();
+			return meme.$query(trx).withGraphFetched("[tags, template, texts.[textBlock]]");
+		}),
 	},
 };
