930ababb859d56f787d26bf7b52844028dfb68cb
Greg Arnold <greg@arnoldassociates.com>
2025-05-01 23:34:35 -0400

    Update workers with DB connection info and process job better.

--- backend/src/workers/ai-classification-worker.ts
+++ backend/src/workers/ai-classification-worker.ts
@@ -2,26 +2,29 @@ import {AIClassifier} from "../services/ai-classification";
 import {Task} from "../models/task";
 import {IsolationLevel, transaction} from "../middleware/transaction";
 import {Joined} from "../utils/relation-types";
+import "../config/database";
 
 const service = new AIClassifier();
 
 async function processNextTask() {
-	try {
-		const task = await transaction(IsolationLevel.Serializable, async trx => {
-			const task = (await Task.query()
-				.where("action", "process")
-				.orderBy("created_at", "asc")
-				.withGraphFetched("meme")
-				.first()) as Joined<Task, "meme">;
-			if (!task) return;
-			await task.$query(trx).patch({status: "processing"});
-			return task;
-		});
-
+	const task = await transaction(IsolationLevel.Serializable, async trx => {
+		const task = (await Task.query()
+			.where("action", "process")
+			.orderBy("created_at", "asc")
+			.withGraphJoined("meme")
+			.limit(1)
+			.first()) as Joined<Task, "meme">;
 		if (!task) return;
+		await task.$query(trx).patch({status: "processing"});
+		return task;
+	});
+	if (!task) return;
+
+	try {
 		await task.meme.process(service);
 		await Task.query().deleteById(task.id);
 	} catch (error) {
+		await task.$query().patch({status: "failed"});
 		console.error("Error processing task:", error);
 	}
 }
